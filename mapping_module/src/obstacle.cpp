cfbb3321a360413fe0c6f6e887ee00ddf6f31e17
Mohit Kulkarni <mohitmk@iitk.ac.in>
2020-04-07 14:14:33 +0530

    Added Well and Gate

--- mapping_module/src/obstacle.cpp
+++ mapping_module/src/obstacle.cpp
@@ -7,3 +7,15 @@ double wall :: potential(const vect& p) {
 	return gauss;
 }
 
+
+double well :: potential(const vect& p) {
+	double d = (p-center).length()
+	double gauss = _gaussian(max(d-r, 0), 0.7071);
+	return gauss;
+}
+
+double door :: potential(const vect& p) {
+	double d = abs((p-f1).length() - (p-f2).length());
+	double gauss = _gaussian(max(d- std::sqrt(w*w+h*h), 0), 0.7071);
+	return gauss;
+}
