da161c2ed2eb950e3cf49a0b6861be5889caa637
catchmeifyocan <shiven@iitk.ac.in>
2020-04-08 17:12:36 +0530

    total_field done

--- mapping_module/src/potential_field.cpp
+++ mapping_module/src/potential_field.cpp
@@ -17,13 +17,41 @@ void bot :: update_bot_orientation(const double& rot_angle){
 	return;
 }
 
-double field :: total_potential(const vect& p){
-	//run over all obstacles in the field -_- 
-	double potential = 0;
-	for (auto obstacle : obstacles){
-		potential += obstacle.potential(p);	
-		//should we use integral over an area instead?
+// double field :: total_potential(const vect& p){
+// 	//run over all obstacles in the field -_- 
+// 	double potential = 0;
+// 	for (auto obstacle : obstacles){
+// 		potential += obstacle.potential(p);	
+// 		//should we use integral over an area instead?
+// 	}
+// 	return potential;
+// }
+double field :: total_potential(const vect& p,double size,int n){
+
+	double potential=0;
+	int i,j,k;
+	int unit=int(size/n)
+	//vect temp=vect make_vect(p.x,p.y);
+	//object obstacle;
+	//std::vector <object*> Obstacles;
+	int l=Obstacles.size();
+	
+	
+	for( k=0;k<l;k++){
+
+		
+		for( i=0 ;i<int(size/2) ;i+=unit ){
+
+			for( j=0; j<int(size/2); j+=unit ){
+			
+				potential+=Obstacles[k]->potential(make_vect( p.x-i,p.y-j));
+				potential+=Obstacles[k]->potential(make_vect( p.x+i,p.y+j));
+				potential+=Obstacles[k]->potential(make_vect( p.x-i,p.y+j));
+				potential+=Obstacles[k]->potential(make_vect( p.x+i,p.y-j));
+			}
+		}
 	}
+
 	return potential;
 }
 
