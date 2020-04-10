fbe34180fa641f0f53ad0fe0d50fa25cb2473ea2
catchmeifyocan <shiven@iitk.ac.in>
2020-04-11 00:34:56 +0530

    merged, properly

--- mapping_module/src/obstacle.cpp
+++ mapping_module/src/obstacle.cpp
@@ -1,26 +1,74 @@
-//#include<map.hpp>
-#include<obstacle.hpp>
+#include<vect.hpp>
+#include<functions.hpp>
 #include<cmath>
 #include<iostream>
+#include<obstacle.hpp>
+
 namespace huroiitk{
-	
+const double k;
 vect wall :: potential(const vect& p, METHOD Method) {
+
 	double d = (bp1 - p).length() + (bp2 - p).length();
+	switch (Method)
+	{
+	case METHOD::GAUSSIAN:
+	{
 	double gauss = _gaussian(d - std::sqrt(w*w + t*t), 0.7071);
 	return -gauss*((center-p)/(center-p).length());
+	}
+		break;
+	case METHOD::INVERSE:
+	{
+		double inverse=_inverse(d,k);
+		return inverse;
+	}
+		break;
+	// default:
+	// 	break;
+	}
+	
 }
 
 
 vect well :: potential(const vect& p, METHOD Method) {
 	double d = (p-center).length();
+	switch (Method)
+	{
+	case METHOD::GAUSSIAN:
+	{
 	double gauss = _gaussian(std::max(d-r, 0.0), 0.7071);
 	return -gauss*((center-p)/(center-p).length());
+	}
+		break;
+	case METHOD::INVERSE:
+	{
+		double inverse= _inverse(d,k);
+		return inverse;
+	}
+		break;	
+	// default:
+	// 	break;
+	}
+	
 }
 
 vect door :: potential(const vect& p, METHOD Method) {
 	double d = (p-center).length();
+	switch (Method)
+	{
+	case METHOD::GAUSSIAN:
+	{
 	double gauss = (d*d)*_gaussian(d, w/(std::pow(2, 1.5)))/(w*w); //sigma will most probably be w/(2^(1.5))
 	return -gauss*((center-p)/(center-p).length());
+	}
+		break;
+	case METHOD::INVERSE:
+	{
+		double inverse=_inverse(d,k);
+		return inverse;
+	}
+		break;
+	}
 }
 
 double goal_line :: distance(const vect& p) {
@@ -33,9 +81,5 @@ vect goal_line :: potential(const vect& p, METHOD method){
 	vect normal = make_vect(-std::sin(orient), std::cos(orient));
 	return 0.5*normal;
 }
-inline double _inverse(const double d, const double k){
-	double eps = 1e-9;
-	double potential = k/(d+eps);
-	return potential;
-}
+
 }
